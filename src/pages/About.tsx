86dc309e643e1b605201e984ce45079cda4a1115
Hugo <48784114+ianneshc@users.noreply.github.com>
2023-10-15 23:47:16 +0200

    Edited carousel on About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,18 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../styles/About.css";
 import NavBar from "../components/NavBar";
 
-import FirstImage from "../assets/Images/carrousel (1).jpg";
-import SecondImage from "../assets/Images/carrousel (2).jpg";
-import ThirdImage from "../assets/Images/carrousel (3).jpg";
-import FourthImage from "../assets/Images/carrousel (4).jpg";
+import FirstImage from "../assets/Images/carousel (1).jpg";
+import SecondImage from "../assets/Images/carousel (2).jpg";
+import ThirdImage from "../assets/Images/carousel (3).jpg";
+import FourthImage from "../assets/Images/carousel (4).jpg";
 
-function About() {
+const AboutPage: React.FC = () => {
+  const images = [FirstImage, SecondImage, ThirdImage, FourthImage];
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % images.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [images.length]);
+
   return (
-    <div>
-      <NavBar></NavBar>
-      <h1 className="welcome">sobre nosotros</h1>
-    </div>
+    <main className="about-page">
+      <NavBar />
+      <h1 className="welcome">Sobre Nosotros</h1>
+      <p className="subWelcome">
+        En Casual Entertainment, nos dedicamos a transformar hogares en espacios
+        extraordinarios y funcionales que reflejan la esencia de cada cliente.
+        Nuestro enfoque meticuloso en cada detalle y nuestra pasión por el
+        diseño excepcional nos distinguen en el mundo de la reforma de hogares.
+        Permita que nuestros expertos guíen el proceso y hagan realidad su
+        visión de un hogar único y distinguido.
+      </p>
+      <section className="carousel-container">
+        <div
+          className="carousel"
+          style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+        >
+          {images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Reforma de Hogar - Imagen ${index + 1}`}
+            />
+          ))}
+        </div>
+      </section>
+    </main>
   );
-}
+};
 
-export default About;
+export default AboutPage;
